6da7135600a395040b70814db26c69931279ae43
Dmitry Alexeev <dalexeev.work@gmail.com>
2021-03-23 10:33:29 +0300

    v 1.0

--- Lab1/main.py
+++ Lab1/main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from integrators import TRungeKutta
 from models import TSpaceCraft
 
+
 def main():
     integrator = TRungeKutta()
     model = TSpaceCraft()
@@ -19,21 +20,40 @@ def main():
     y = []
     z = []
     for i in np.arange(t, tk): 
+        if i % 1000 == 0:
+            print('\r', end='')
+            print('Progress: %.1f' % (i/tk*100), '%', end='')
         integrator.oneStep(model, values, t, h)
         x.append(values[0])
         y.append(values[1])
         z.append(values[2])
         t += h
-        print(i, end='\n')
 
+    # запись координат в файлы
+    fx = open('x.txt', 'w')
+    fy = open('y.txt', 'w')
+    fz = open('z.txt', 'w')
+    for i in range(len(x)):
+        fx.write(str(x[i]) + '\n')
+        fy.write(str(y[i]) + '\n')
+        fz.write(str(z[i]) + '\n')
+    fx.close()
+    fy.close()
+    fx.close()
+    
+    # вывод графика в файл
+    plt.title('Зависимость координат от времени')
     plt.plot(tt, x, label='x')
     plt.plot(tt, y, label='y')
     plt.plot(tt, z, label='z')
-    plt.xlabel('t - axis')
+    plt.xlabel('t')
     plt.legend()
+    plt.savefig('result.png')
     plt.show()
 
-
+    print('Done')
 
 if __name__ == "__main__":
+    print('Program start')
     main()
+    print('Program end')
